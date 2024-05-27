ac826b1f3c1914c25726810cfacd078d4986f01f
anlegent <alegent@edenschool.fr>
2024-05-27 17:15:20 +0200

    register form +

---
+++ main.js
@@ -0,0 +1,10 @@
+const container = document.getElementById("#container");
+const article = document.createElement("article");
+article.classList.add("article");
+container.appendChild(article);
+const img = document.createElement("img");
+img.classList.add("image");
+article.appendChild(img);
+const para = document.createElement("p");
+para.classList.add("paragraph");
+img.appendChild(para);
