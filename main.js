a4e59f9cafa43d96efba3cae3e201a33512a8885
anlegent <alegent@edenschool.fr>
2024-05-28 17:10:13 +0200

    modifications on html js and css

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 const container = document.getElementById("#container");
+
 const article = document.createElement("article");
 article.classList.add("article");
 container.appendChild(article);
+
 const img = document.createElement("img");
 img.classList.add("image");
 article.appendChild(img);
+
 const para = document.createElement("p");
-para.classList.add("paragraph");
+para.classList.add("parag");
 img.appendChild(para);
+
+const articleTitle = (document.getElementsByClassName("parag").textContent =
+  "this text is generated from Js");
