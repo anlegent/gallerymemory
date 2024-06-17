ccb5f66b561ba6d0b071fe7e1520733303dee0a3
anlegent <alegent@edenschool.fr>
2024-06-17 15:13:38 +0200

    articles images and titles js

--- main.js
+++ main.js
@@ -1,76 +1,94 @@
-const gallery = getElementById("container")
+let count = 0
+for (let i = 0; i < 8; i++) {
+    count++
 
-function renderGallery(data) {
-    gallery.innerHTML = ""
-    const ul = document.createElement("ul")
-    for (let i = 0; i < data.length; i++) {
-        const li = document.createElement("li")
-        li.innerHTML = `<img src="${data[i].image}" alt="${data[i].title}"></img>`
-        ul.appendChild(li)
-    }
-    gallery.appendChild(ul)
+    const article = document.createElement("article")
+    article.id = `article${count}`
+    document.body.appendChild(article)
+    document.getElementById("container").appendChild(article)
+
+    const image = document.createElement("img")
+    image.id = "image"
+    image.srcset = "https://picsum.photos/300"
+    document.body.appendChild(image)
+    document.getElementById(`article${count}`).appendChild(image)
+
+    const title = document.createElement("p")
+    title.id = "title"
+    title.textContent = "a title"
+    document.body.appendChild(title);
+    document.getElementById(`article${count}`).appendChild(title)
 }
 
-const search = getElementById("search")
-search.addEventListener("input", () => {
-    const newData = []
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].title.includes(search.value)) {
-            newData.push(data[i])
-        }
-        renderGallery(newData)
-    }
-    renderGallery(data)
-})
 
-
-const data = [
-    {
-        id: 0,
-        image: "https://picsum.photos/id/40/400",
-        title: "Close-up of a cat",
-        category: "animals"
-    },
-    {
-        id: 1,
-        image: "https://picsum.photos/id/112/400",
-        title: "Wheat field",
-        category: "nature"
-    },
-    {
-        id: 2,
-        image: "https://picsum.photos/id/419/400",
-        title: "Tramways",
-        category: "vehicules"
-    },
-    {
-        id: 3,
-        image: "https://picsum.photos/id/551/400",
-        title: "White car in the desert",
-        category: "vehicules"
-    },
-    {
-        id: 4,
-        image: "https://picsum.photos/id/197/400",
-        title: "Railways",
-        category: "railways"
-    },
-    {
-        id: 5,
-        image: "https://picsum.photos/id/35/400",
-        title: "Cactus",
-        category: "nature"
-    },
-    {
-        id: 6,
-        image: "https://picsum.photos/id/237/400",
-        title: "Dog",
-        category: "animals"
-    },
-    {
-        id: 7,
-        image: "https://picsum.photos/id/204/400",
-        title: "Railways on a bridge",
-        category: "railways"
-    }
+const images = [
+    "https://picsum.photos/id/40/300",
+    "https://picsum.photos/id/112/300",
+    "https://picsum.photos/id/419/300",
+    "https://picsum.photos/id/551/300",
+    "https://picsum.photos/id/197/300",
+    "https://picsum.photos/id/35/300",
+    "https://picsum.photos/id/237/300",
+    "https://picsum.photos/id/204/300"
 ]
+
+const titles = [
+    "A cat",
+    "Wheat fields",
+    "Tramways",
+    "White car in the desert",
+    "Train railways",
+    "Cactus",
+    "A dog",
+    "railways on bridge"]
+
+//const data = [
+//    {
+//        id: 0,
+//        image: "https://picsum.photos/id/40/300",
+//        title: "A cat",
+//        category: "animals",
+//    },
+//    {
+//        id: 1,
+//        image: "https://picsum.photos/id/112/300",
+//        title: "Wheat fields",
+//        category: "nature"
+//    },
+//    {
+//        id: 2,
+//        image: "https://picsum.photos/id/419/300",
+//        title: "Tramways",
+//        category: "vehicules"
+//    },
+//    {
+//        id: 3,
+//        image: "https://picsum.photos/id/551/300",
+//        title: "White car in the desert",
+//        category: "vehicules"
+//    },
+//    {
+//        id: 4,
+//        image: "https://picsum.photos/id/197/300",
+//        title: "Train railways",
+//        category: "railway"
+//    },
+//    {
+//        id: 5,
+//        image: "https://picsum.photos/id/35/300",
+//        title: "Cactus",
+//        category: "nature"
+//    },
+//    {
+//        id: 6,
+//        image: "https://picsum.photos/id/237/300",
+//        title: "A dog",
+//        category: "animals"
+//    },
+//    {
+//        id: 7,
+//        image: "https://picsum.photos/id/204/300",
+//        title: "Railways on a bridge",
+//        category: "railway"
+//    }
+//]
